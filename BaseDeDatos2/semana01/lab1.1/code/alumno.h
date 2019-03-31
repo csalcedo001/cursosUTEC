9dd52c19d2ffbe6b8dd49b23bc9a9d39acac5c43
César Salcedo <cesar.salcedo@utec.edu.pe>
2019-03-31 14:21:41 -0500

    Checkpoint

--- BaseDeDatos2/semana01/lab1.1/code/alumno.h
+++ BaseDeDatos2/semana01/lab1.1/code/alumno.h
@@ -11,20 +11,27 @@ typedef struct Alumno {
 	char carrera[MSIZE + 1];
 	
 	Alumno(
-		std::string nombre,
-		std::string apPaterno,
-		std::string apMaterno,
-		std::string carrera
+		// std::string nombre,
+		// std::string apPaterno,
+		// std::string apMaterno,
+		// std::string carrera
 	) :
 		nombre{},
 		apPaterno{},
 		apMaterno{},
 		carrera{}
 	{
-		strcpy(this->nombre, nombre.c_str());
-		strcpy(this->apPaterno, apPaterno.c_str());
-		strcpy(this->apMaterno, apMaterno.c_str());
-		strcpy(this->carrera, carrera.c_str());
+		// strcpy(this->nombre, nombre.c_str());
+		// strcpy(this->apPaterno, apPaterno.c_str());
+		// strcpy(this->apMaterno, apMaterno.c_str());
+		// strcpy(this->carrera, carrera.c_str());
+	}
+	
+	Alumno(Alumno& a) {
+		strcpy(this->nombre, a.nombre);
+		strcpy(this->apPaterno, a.apPaterno);
+		strcpy(this->apMaterno, a.apMaterno);
+		strcpy(this->carrera, a.carrera);
 	}
 } Alumno;
 
--- BaseDeDatos2/semana01/lab1.1/code/aula.h
+++ BaseDeDatos2/semana01/lab1.1/code/aula.h
@@ -2,39 +2,72 @@
 #define AULA
 
 #include <fstream>
+#include <vector>
 #include <iostream>
 
 #include "alumno.h"
 
 #define FNSIZE 100 // Filename size
+#define RSIZE 52 // Register size
 
 class Aula {
 	char filename[FNSIZE + 1];
+	// std::fstream file;
 	
 	public:
 	Aula(std::string s) : filename{} {
-		strcpy(filename, s.c_str());
-	}
-	
-	private:
-	void read(int size, char *container) {
-		std::ifstream file(filename);
-		
-		file.read(container, size);
-		
-		file.close();
+		strcpy(this->filename, s.c_str());
+		// this->file.open(this->filename, std::ios::in| std::ios::out);
 	}
 	
 	public:
+	int size() const {
+		std::ifstream file(this->filename);
+		
+		file.seekg(0, std::ios::end);
+		int fileSize = file.tellg() / (RSIZE + 1);
+		
+		file.close();
+		
+		return fileSize;
+	}
+	
 	void load() {
-		int size = 15;
+		std::ifstream file(filename);
 		
-		char s[size + 1];
-		s[size] = 0;
+		int size = this->size();
 		
-		this->read(size, s);
+		Alumno a[size];
 		
-		std::cout << s << std::endl;
+		int pos = 0;
+		
+		do {
+			file.read(a[pos].nombre, 12);
+			file.read(a[pos].apPaterno, 12);
+			file.read(a[pos].apMaterno, 12);
+			file.read(a[pos].carrera, 15);
+			file.seekg(2, std::ios::cur);
+			
+			++pos;
+		} while (pos < size);
+		
+		std::cout << a[0].nombre << std::endl;
+		std::cout << a[0].apPaterno << std::endl;
+		std::cout << a[0].apMaterno << std::endl;
+		std::cout << a[0].carrera << std::endl;
+		
+		std::cout << a[1].nombre << std::endl;
+		std::cout << a[1].apPaterno << std::endl;
+		std::cout << a[1].apMaterno << std::endl;
+		std::cout << a[1].carrera << std::endl;
+		
+		std::cout << a[2].nombre << std::endl;
+		std::cout << a[2].apPaterno << std::endl;
+		std::cout << a[2].apMaterno << std::endl;
+		std::cout << a[2].carrera << std::endl;
+		
+		
+		file.close();
 	}
 	
 	bool add(Alumno a);
