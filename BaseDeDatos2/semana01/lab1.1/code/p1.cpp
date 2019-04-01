028a662d78679a7450687c0d4bac08a24bd11420
Ricardo Salcedo <ricardosalcedo@MacBook.local>
2019-03-31 21:00:16 -0500

    Updated p1.cpp to show results of loading and writing

--- BaseDeDatos2/semana01/lab1.1/code/p1.cpp
+++ BaseDeDatos2/semana01/lab1.1/code/p1.cpp
@@ -6,22 +6,44 @@
 using namespace std;
 
 int main (void) {
-	Aula aula("datos.txt");
+	Aula aula("copiaDatos.txt");
 	
 	std::vector<Alumno> alumnos = aula.load();
 	
+	cout << "----------------------------------------" << endl;
+	cout << "Lectura de datos previa a inserción" << endl;
+	cout << "----------------------------------------" << endl;
+	
 	for (auto a : alumnos) {
-		cout << "Nombre: " << a.nombre << endl;
+		cout << "Nombre:           " << a.nombre << endl;
 		cout << "Apellido paterno: " << a.apPaterno << endl;
 		cout << "Apellido materno: " << a.apMaterno << endl;
-		cout << "Carrera: " << a.carrera << endl << endl;
+		cout << "Carrera:          " << a.carrera << endl;
+		cout << "---------------------------------" << endl;
 	}
+	
+	cout << endl << endl;
 	
 	Alumno a;
 	
 	a.update("Cesar", "Salcedo", "Castillo", "Computacion");
 	
 	aula.add(a);
+	
+	
+	alumnos = aula.load();
+	
+	cout << "----------------------------------------" << endl;
+	cout << "Lectura de datos después de inserción" << endl;
+	cout << "----------------------------------------" << endl;
+	
+	for (auto a : alumnos) {
+		cout << "Nombre:           " << a.nombre << endl;
+		cout << "Apellido paterno: " << a.apPaterno << endl;
+		cout << "Apellido materno: " << a.apMaterno << endl;
+		cout << "Carrera:          " << a.carrera << endl;
+		cout << "---------------------------------" << endl;
+	}
 	
 	return 0;
 }
